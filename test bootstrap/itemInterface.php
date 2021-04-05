20ea16e5166dcea700ba8c61488e7b86185e4ed2
BrcBenjamin <61540777+BrcBenjamin@users.noreply.github.com>
2021-04-05 14:50:59 +0200

    Update itemInterface.php

--- test bootstrap/itemInterface.php
+++ test bootstrap/itemInterface.php
@@ -102,6 +102,12 @@
     $mysqli->close();
 ?>
 
-<?php include "./footer.php" ?>
+<footer class="container-fluid pt-3 bg-dark text-white" style="position:absolute; bottom:0;">
+    <div class="container">
+      <p class="float-end"><a class="text-white" href="#">Back to top</a></p>
+      <p>© 2017–2021 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+    </div>
+    
+  </footer>
 </body>
 </html>
