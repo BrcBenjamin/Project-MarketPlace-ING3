323bd8c87d5f6cca2d2599e6f3e497127545d563
chemsss <61418782+chemsss@users.noreply.github.com>
2021-04-05 02:08:44 +0200

    include config.php

--- test bootstrap/basket.php
+++ test bootstrap/basket.php
@@ -34,22 +34,11 @@
         
 
     <?php
-        /*
-        * Change the value of $password if you have set a password on the root userid
-        * Change NULL to port number to use DBMS other than the default using port 3306
-        *
-        */
+        
         $email = $_GET['id'];
-        $user = 'root';
-        $password = ''; //To be completed if you have set a password to root
-        $database = 'yourmarket'; //To be completed to connect to a database. The database must exist.
-        $port = 3308; //Default must be NULL to use default port
-        $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
 
-        if ($mysqli->connect_error) {
-            die('Connect Error adzDZADAZDAZ (' . $mysqli->connect_errno . ') '
-                    . $mysqli->connect_error);
-        }
+        include "config.php";
+
         $sql = "SELECT * FROM basketitem, item WHERE fk_email='" .$email ."' AND fk_iditem=iditem AND (purchaseCategory=1 OR purchaseCategory=3)";
         $result = $mysqli->query($sql);
         //echo "number of row".$result->num_rows;
