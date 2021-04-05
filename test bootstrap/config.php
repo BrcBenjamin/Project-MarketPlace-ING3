b4b12dbac6e5330d67b433b166276cc01a106f64
AbbasJr <bibesj@yahoo.fr>
2021-04-05 11:58:10 +0200

    change

--- test bootstrap/config.php
+++ test bootstrap/config.php
@@ -7,7 +7,7 @@
     $user = 'root';
     $password = ''; //To be completed if you have set a password to root
     $database = 'yourmarket'; //To be completed to connect to a database. The database must exist.
-    $port = 3308; //Default must be NULL to use default port
+    $port = null; //Default must be NULL to use default port
     $mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
 
     if ($mysqli->connect_error) {
